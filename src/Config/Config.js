d384caa7a73157a2bd198dc9b1c59b9fd7879835
Mahima Kaushik <59880406+mahimakaushik-oss@users.noreply.github.com>
2023-01-26 22:27:08 +0530

    details removed

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -4,12 +4,12 @@ import 'firebase/firestore'
 import 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA0S6BQc7WXCUn1TGedNkPD791TQeibUko",
-  authDomain: "mytarscano.firebaseapp.com",
-  projectId: "mytarscano",
-  storageBucket: "mytarscano.appspot.com",
-  messagingSenderId: "827860013540",
-  appId: "1:827860013540:web:943b68fba9dcf75326b7f1"
+  apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  authDomain: "XXXXXXXXXXXXXXXXXXXXX",
+  projectId: "XXXXXXXXXXXX",
+  storageBucket: "XXXXXXXXXXXXXXXX",
+  messagingSenderId: "XXXXXXXXXXX",
+  appId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 };
 
 firebase.initializeApp(firebaseConfig);
